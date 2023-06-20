9b272d2f0b15dd76f77cefd9991dd942b8805e39
+ <+>
2023-06-20 21:37:01 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
-function addingEventListener() {
-    let input=document.querySelector('#button');
-    input.addEventListener('click',()=>{});
+// const input = document.getElementById('input');
+// input.addEventListener('click', function() {
+//   alert('I was clicked!');
+// });
+ 
+// const input = document.getElementById('input');
+
+// function clickAlert() {
+//   alert('I was clicked!');
+// }
+
+// input.addEventListener('click', clickAlert);
+
+
+//create a function addingEventlistener and add addEventlistener inside then call the outer function
+
+function addingEventListener(){
+  const input = document.getElementById('button');
+  input.addEventListener('click',function(){
+    alert ('I was clicked!');
+  });
 }
+addingEventListener();
